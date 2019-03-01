3748b0d921099a232f0308164faf35fda7046ec9
Ralph Schoon <rsjazz777@gmail.com>
2019-03-01 09:36:44 +0100

    Initial Sharing

---
+++ com.ibm.js.team.monitoring.custom.expensivescenario/src/com/ibm/js/team/monitoring/custom/expensivescenario/ExpensiveScenarioNotifier.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2019 IBM Corporation
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ *******************************************************************************/
+package com.ibm.js.team.monitoring.custom.expensivescenario;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import com.ibm.js.team.monitoring.custom.service.expensivescenario.ExpensiveScenarioService;
+import com.ibm.team.repository.client.ITeamRepository;
+import com.ibm.team.repository.client.ITeamRepository.ILoginHandler;
+import com.ibm.team.repository.client.ITeamRepository.ILoginHandler.ILoginInfo;
+import com.ibm.team.repository.client.TeamPlatform;
+import com.ibm.team.repository.common.TeamRepositoryException;
+
+public class ExpensiveScenarioNotifier {
+
+	private static class LoginHandler implements ILoginHandler, ILoginInfo {
+
+		private static String fPassword;
+		private static String fUserId;
+
+		public LoginHandler(final String userId, final String password) {
+			LoginHandler.fUserId = userId;
+			LoginHandler.fPassword = password;
+		}
+
+		@Override
+		public ILoginInfo challenge(final ITeamRepository repository) {
+			return this;
+		}
+
+		@Override
+		public String getPassword() {
+			return LoginHandler.fPassword;
+		}
+
+		@Override
+		public String getUserId() {
+			return LoginHandler.fUserId;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		boolean result = false;
+		TeamPlatform.startup();
+		try {
+			result = run(args);
+		} catch (TeamRepositoryException e) {
+			System.out.println("TeamRepositoryException: " + e.getMessage());
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			System.out.println("URISyntaxException: " + e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			TeamPlatform.shutdown();
+		}
+		if (!result) {
+			System.out.println("Failed.");
+			System.exit(1);
+		}
+		System.out.println("Success.");
+	}
+
+	private static boolean run(String[] args) throws Exception {
+
+		if (args.length != 5) {
+			System.out
+					.println("Usage: esn [publicURI] [userId] [password] start [scenarionName]");
+			System.out.println("Usage: esn [publicURI] [userId] [password] stop [scenarionName]");
+			return false;
+		}
+
+		final String publicURI = args[0];
+		final String userId = args[1];
+		final String password = args[2];
+		final String startStop = args[3];
+		final String scenarioName = args[4];
+
+		// log in to platform
+		ITeamRepository teamRepository = TeamPlatform.getTeamRepositoryService().getTeamRepository(publicURI);
+		IProgressMonitor monitor = new NullProgressMonitor();
+		teamRepository.registerLoginHandler(new LoginHandler(userId, password));
+		teamRepository.login(monitor);		
+		ExpensiveScenarioService.runService(teamRepository, teamRepository.getRepositoryURI(), scenarioName, startStop);
+		teamRepository.logout();
+		return true;
+	}
+
+}
---
+++ com.ibm.js.team.monitoring.custom.service.expensivescenario/src/com/ibm/js/team/monitoring/custom/service/expensivescenario/FilePersitentExpensiveScenarioService.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2019 IBM Corporation
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ *******************************************************************************/
+package com.ibm.js.team.monitoring.custom.service.expensivescenario;
+
+import java.io.FileWriter;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Service to execute a start and stop for an expensive scenario. 
+ * The information to stop the expensive scenario is persisted as a file.
+ *
+ */
+public class FilePersitentExpensiveScenarioService {
+
+	public static final String SCENARIO_RESPONSE_FILE_EXTENSION = ".json";
+	public static final String SCENARIO_RESPONSE_FILE_PATH_ROOT_FOLDER = "./";
+
+	private IExpensiveScenarioService fExpensiveScenarioService=null;
+
+	/**
+	 * Start and stop expensive scenario counter are performed persisting the
+	 * scenario counter in a file or pass it as string. See option
+	 * persistStartAsFile.
+	 * 
+	 * @param teamRepository     Team Repository
+	 * @param publicURI          Public URI of the target CLM server
+	 * @param scenarioName       the name of the scenario
+	 * @throws URISyntaxException
+	 */
+	public FilePersitentExpensiveScenarioService(IExpensiveScenarioService expensiveScenarioService) throws URISyntaxException {
+		if(expensiveScenarioService==null)
+			throw new NullPointerException("Expensive Scenario Service can not be null");
+		fExpensiveScenarioService = expensiveScenarioService;
+	}
+
+
+	/**
+	 * Start the custom expensive scenario.
+	 * Store the request response data in a file to persist it.
+	 *  
+	 * @throws Exception
+	 */
+	public void start() throws Exception {
+		String responseString = fExpensiveScenarioService.start();
+		FileWriter fileWriter = new FileWriter(getScenarioResponseFileName());
+		fileWriter.write(responseString);
+		fileWriter.close();
+	}
+
+	/**
+	 * Stop the custom expensive scenario.
+	 * Read the request response data from a file that persists it.
+	 * 
+	 * @throws Exception
+	 */
+	public void stop()
+			throws Exception {
+		Path path = Paths.get(getScenarioResponseFileName());
+		String startRequestResponse = new String(Files.readAllBytes(path));
+		Files.delete(path);
+		fExpensiveScenarioService.stop(startRequestResponse);
+	}
+
+	/**
+	 * @return the name of the file to contain the request respones information
+	 * @throws Exception 
+	 */
+	private String getScenarioResponseFileName() throws Exception {
+		return SCENARIO_RESPONSE_FILE_PATH_ROOT_FOLDER + fExpensiveScenarioService.getScenarioName() + SCENARIO_RESPONSE_FILE_EXTENSION;
+	}
+}
